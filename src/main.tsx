2cdd0acd2ebceacc33f070250ab264e8b24cbbe5
unknown <haddadiamirhosein.d@gmail.com>
2025-05-02 12:01:25 +0330

    build color mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-    <App />
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
 )
