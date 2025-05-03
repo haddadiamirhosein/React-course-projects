1c74be957f98e0ebfd546394e40e0e487c3aa7b2
unknown <haddadiamirhosein.d@gmail.com>
2025-05-03 19:29:31 +0330

    Displaing platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ function GameGrid() {
   return (
     <>
     {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm:1 , md:2 , lg:3 , xl:5 }} spacing={20}>
+    <SimpleGrid columns={{sm:1 , md:2 , lg:3 , xl:5 }} padding='10px' spacing={20}>
         {games.map(game => (
             <GameCard key={game.id} game={game} />
         ))}
