cdb844d360e37e2917f2288c289e6f60c066e609
unknown <haddadiamirhosein.d@gmail.com>
2025-05-04 22:47:52 +0330

    Remove Duplicated Css

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,21 @@ function App() {
       base : `"nav" "main"`,
       lg : `"nav nav" "aside main"`,
     }}>
+      
       <GridItem area="nav" >
         <NavBar></NavBar>
       </GridItem>
+
       <Show  above='lg'>
         <GridItem area="aside">
           aside
         </GridItem>
       </Show>
+
       <GridItem area="main">
         <GameGrid></GameGrid>
       </GridItem>
+
     </Grid>
   )
 }
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
 
 
 function GameGrid() {
@@ -15,11 +16,17 @@ function GameGrid() {
     <SimpleGrid columns={{sm:1 , md:2 , lg:3 , xl:5 }} padding='10px' spacing={20}>
 
       {isloading && skeletons.map(item => (
-        <GameCardSkeleton key={item}/>
+
+        <GameCardContainer>
+          <GameCardSkeleton key={item}/>
+        </GameCardContainer> 
+
       ))}
 
       {games.map(game => (
-        <GameCard key={game.id} game={game} />
+        <GameCardContainer>
+          <GameCard key={game.id} game={game} />
+        </GameCardContainer>
       ))}
 
 
